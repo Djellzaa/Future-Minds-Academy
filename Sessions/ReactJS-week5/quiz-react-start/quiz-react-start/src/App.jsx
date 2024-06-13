826b0a2e12eceaf024cc601999b2fd964379dbae
Djellzaa <djellzahajrullahu@gmail.com>
2024-06-13 20:14:00 +0200

    .

---
+++ Sessions/ReactJS-week5/quiz-react-start/quiz-react-start/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
+import QuestionTimer from "./components/QuizTimer";
+
+function App() {
+    return(
+        <>
+            <Header/>
+            <Quiz/>
+            <QuestionTimer/>
+        </>
+        )
+}
+
+
+export default App;
